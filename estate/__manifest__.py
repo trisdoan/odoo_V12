f07cc5d1c8e78fb501f18d253c2a3d6d0ec12610
tris <doanminhtri8183@gmail.com>
2023-02-05 12:25:16 +0700

    finish chap 13

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -19,6 +19,7 @@
         "security/ir.model.access.csv",
         "views/estate_menus.xml",
         "views/estate_property_views.xml",
+        "views/res_users_form.xml",
 
     ],
     # only loaded in demonstration mode
