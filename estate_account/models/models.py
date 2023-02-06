4b2d84afd5e82cf92ad340e51f1103812fa43324
Tris Doan <doanminhtri8183@gmail.com>
2023-02-06 17:37:07 +0700

    finish chapter 14

--- estate_account/models/models.py
+++ estate_account/models/models.py
@@ -7,41 +7,40 @@ class Property(models.Model):
 
     # override sold action
     def set_sold(self):
+        # if self.env.user.has_group('account.group_account_invoice'):
+        #     # check if the user is in group billing
+        #     try:
+        #         self.check_access_rights('write')
+        #         self.check_access_rights('create')
+        #     except AccessError:
+        #         raise UserError(_("You don't have the access rights needed to update or create a invoice"))
+        #
+        #     # check access rules
+        #     self.check_access_rule('create')
 
-        if self.env.user.has_group('account.group_account_invoice'):
-            # check if the user is in group billing
-            try:
-                self.check_access_rights('write')
-                self.check_access_rights('create')
-            except AccessError:
-                raise UserError(_("You don't have the access rights needed to update or create a invoice"))
+        journal = self.env['account.journal'].search([('type', '=', 'sale')])[0]
 
-            # check access rules
-            self.check_access_rule('create')
+        # create invoice line
+        invoice_lines = [
+            {
+                "name": self.name,
+                "quantity": 1,
+                "price_unit": (self.selling_price * 6) / 100
+            },
+            {
+                "name": "Administrative fees",
+                "quantity": 1,
+                "price_unit": 100
+            }
+        ]
+        print(" reached ".center(100, '='))
+        self.env['account.move'].sudo().create({
+            'partner_id': self.buyer.id,
+            'move_type': "out_invoice",
+            'journal_id': journal.id,
+            'invoice_line_ids': invoice_lines
+        })
 
-            journal = self.env['account.journal'].search([('type', '=', 'sale')])[0]
-
-            # create invoice line
-            invoice_lines = [
-                {
-                    "name": self.name,
-                    "quantity": 1,
-                    "price_unit": (self.selling_price * 6) / 100
-                },
-                {
-                    "name": "Administrative fees",
-                    "quantity": 1,
-                    "price_unit": 100
-                }
-            ]
-            print(" reached ".center(100, '='))
-            account_move = self.env['account.move'].sudo().create({
-                'partner_id': self.buyer.id,
-                'move_type': "out_invoice",
-                'journal_id': journal.id,
-                'invoice_line_ids': invoice_lines
-            })
-
-            return super().set_sold()
-        else:
-            raise UserError(_("You don't have the access needed"))
+        return super().set_sold(self)
+        # else:
+        #     raise UserError(_("You don't have the access needed"))
