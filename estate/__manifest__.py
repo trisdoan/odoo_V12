e185a1b708c88b82d0d3d04a0ef6bcfd35e9de2b
tris <doanminhtri8183@gmail.com>
2023-02-10 11:24:33 +0700

    add access right for group users

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -17,6 +17,7 @@
     # always loaded
     'data': [
         "security/ir.model.access.csv",
+        "security/security_groups.xml",
         "views/estate_menus.xml",
         "views/estate_property_views.xml",
         "views/res_users_form.xml",
